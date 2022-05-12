b5b5adfe9afb982c3c8196f63dd999c86b667c5d
Neto Queiroz <queirozneto94@hotmail.com>
2022-05-11 21:04:39 -0300

    mexendo com propriedades

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Comentario from './components/comentarios';
+import Comentario from './components/Comentario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu projeto</h1>
-      <Comentario nome="joao" email="joão@email.com" data={new Date(2022, 5, 9)}>
-        Olá, tudo bem?
-      </Comentario>
+class App extends Component {
 
-      <Comentario nome="maria" email="maria@email.com" data={new Date(2022, 5, 11)}>
-        Olá, tudo bem sim.
-      </Comentario>
-    </div>
-  );
+  state = {
+    comentarios: [
+      {
+        nome: 'João',
+        email: 'joão@mail.com',
+        data: new Date(2022, 5, 9),
+        mensagem: 'Olá, tudo bem?'
+      },
+      {
+        nome: 'pedro',
+        email: 'pedro@mail.com',
+        data: new Date(2022, 5, 11),
+        mensagem: 'Olá, tudo bem sim...'
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Meu projeto</h1>
+        {this.state.comentarios.map((comentario, indice) => (
+          <Comentario
+            key={indice} //se usar do banco, usa IdleDeadline.
+            nome={comentario.nome}
+            email={comentario.email}
+            data={comentario.data}>
+            {comentario.mensagem}
+          </Comentario>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
